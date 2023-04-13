53832ba8e93da01e0ed869fb1a7bdb2c2206d0a3
NjeriKiarie <kcarolnjeri@gmail.com>
2023-04-13 13:10:02 -0700

    Square

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const myArg = process.argv.length;
+
+if (myArg < 3)
+{
+	console.log("No argument");
+}
+else if (myArg === 3)
+{
+	console.log("Argument found");
+}
+else {
+	console.log("Argument found");
+}
