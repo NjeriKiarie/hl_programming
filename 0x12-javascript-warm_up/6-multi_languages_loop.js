991bdb6a46a88ae213990b1d1b61323266b96610
NjeriKiarie <kcarolnjeri@gmail.com>
2023-04-18 22:16:12 -0700

    alx-higher-level

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0, len = language.length; i < len; i++) {
-  console.log(language[i]);
-}
--- 0x12-javascript-warm_up/README.md
+++
@@ -1,3 +0,0 @@
-0x12-javascript-warm_up
-0. First constant, first print
-Write a script that prints “JavaScript is amazing”:
