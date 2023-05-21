90d9d63ca03459ef3474b10e2fc07a9fc82821f4
NjeriKiarie <kcarolnjeri@gmail.com>
2023-05-21 03:44:05 -0700

    3-9 Full Creation

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- a script that lists all the tables of a database in your MySQL server.
+-- the database name to be passed as an argument of the mysql command
 
-USE mysql;
 SHOW TABLES;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- new row id=89, name= Best school
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO first_table VALUES(89, 'Best School');
