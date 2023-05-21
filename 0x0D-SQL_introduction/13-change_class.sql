3df71b2221df1f938ae26f4e15eb4386f262aebd
NjeriKiarie <kcarolnjeri@gmail.com>
2023-05-21 05:53:24 -0700

    Score too low

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,9 @@
+-- a script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT * 
+FROM second_table 
+WHERE score <= 5;
+
+DELETE FROM  second_table
+WHERE score <= 5;
