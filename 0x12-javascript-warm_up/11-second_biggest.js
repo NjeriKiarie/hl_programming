74d9c02e669642e791a876012b8fbee47c5a36bd
NjeriKiarie <kcarolnjeri@gmail.com>
2023-07-19 03:51:26 -0700

    Increment

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length > 3) {
   const array = process.argv.slice(2).map(Number);
 
-  array.slice(array.indexOf(Math.max.apply(null, array)), 1);
+  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
   console.log(Math.max.apply(null, array));
 } else {
   console.log(0);
